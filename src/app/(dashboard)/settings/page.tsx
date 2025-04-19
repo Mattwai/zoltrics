a3fca350d7c4da310381c0226e53d4ed45429afc
MattWai <39748665+Mattwai@users.noreply.github.com>
2025-04-20 01:25:18 +1200

    fix booking link in settings

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -3,18 +3,32 @@ import BillingSettings from "@/components/settings/billing-settings";
 import BookingLink from "@/components/settings/booking-link";
 import ChangePassword from "@/components/settings/change-password";
 import DarkModetoggle from "@/components/settings/dark-mode";
+import { authConfig } from "@/lib/auth";
+import prisma from "@/lib/prisma";
+import { getServerSession } from "next-auth";
 
 type Props = {};
 
-const Page = (props: Props) => {
+const Page = async (props: Props) => {
+  const session = await getServerSession(authConfig);
+  if (!session || !session.user) return null;
+
+  // Fetch user details including booking link
+  const user = await prisma.user.findUnique({
+    where: { id: session.user.id },
+    select: { bookingLink: true }
+  });
   return (
     <>
       <InfoBar />
-      <div className="overflow-y-auto w-full chat-window flex-1 h-0 flex flex-col gap-10">
+      <div className="overflow-y-auto w-full chat-window flex-1 h-0 flex flex-col gap-10 px-2">
         <BillingSettings />
-        <BookingLink />
+        <BookingLink               
+          userId={session.user.id}
+          initialBookingLink={user?.bookingLink || null}
+          baseUrl={process.env.NEXT_PUBLIC_BASE_URL || ""}/>
         <DarkModetoggle />
-        <ChangePassword />
+        {/* <ChangePassword /> */}
       </div>
     </>
   );
